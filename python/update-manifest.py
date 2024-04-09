b040a46b812ffb495e6fd061cf9aa2faef301023
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2024-04-09 13:54:21 +0200

    Hent info om run i selector Co-authored-by: Amirhossien Lotfi <amirhossien.lotfi@nav.no> Co-authored-by: Sergio Alberto Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>

--- python/update-manifest.py
+++ python/update-manifest.py
@@ -1,6 +1,9 @@
 import sys
 import argparse
+import time
+
 import requests.auth
+import random
 
 
 class BearerAuth(requests.auth.AuthBase):
@@ -24,8 +27,8 @@ parser.add_argument("token")
 args = parser.parse_args()
 
 action_url = "https://api.github.com/repos/navikt/tms-mikrofrontend-selector/dispatches"
+dispatch_id = random.randint(1000, 9999)
 
-# sette opp payload
 payload = {
     "event_type": "update_microfrontend_manifest",
     "client_payload": {
@@ -33,27 +36,56 @@ payload = {
         "url": args.url,
         "cluster": args.cluster,
         "initiator": args.initiator,
-        "commitmsg": args.message
-    }}
+        "commitmsg": args.message,
+        "dispatch_id": dispatch_id
+    }
+}
+
 headers = {
     "Accept": "application/vnd.github+json",
     "User-Agent": "request"
 }
 
-
 try:
     response = requests.post(action_url, json=payload, headers=headers, auth=BearerAuth(args.token))
     response.raise_for_status()
-    print(response.content)
 except requests.exceptions.HTTPError as error:
     print(error)
     print(response.headers)
     sys.exit(1)
 
 print("Oppdatering av manifest startet")
-print(response.content)
-print(response.text)
-print(response.headers)
-print(response.json())
 
+run_name = "Oppdater {0} i {1} : {2}  {3}".format(
+    args.id, args.cluster, args.commitmsg, dispatch_id
+)
 
+try:
+    time.sleep(60)
+    response = requests.get("https://api.github.com/repos/navikt/tms-mikrofrontend-selector/actions/runs",
+                            headers=headers, auth=BearerAuth(args.token))
+    response.raise_for_status()
+    run_name = "Oppdater tms-shell i dev-gcp : dev-gcp"
+    workflows = response.json()["workflow_runs"]
+    workflow_id = filter(lambda x: x["name"] == run_name, workflows).__next__()["id"]
+
+    response = requests.get(
+        "https://api.github.com/repos/navikt/tms-mikrofrontend-selector/actions/runs/{}".format(workflow_id),
+        headers=headers, auth=BearerAuth(args.token))
+    response.raise_for_status()
+    status = response.json()["conclusion"]
+    if status == "success":
+        print("Manifesteturl er oppdatert! ")
+        sys.exit(0)
+    elif status == "failure":
+        print("Manifestoppdatering feiler i tms-mikrofrontend-selector, kontakt team min side")
+        sys.exit(1)
+    else:
+        print("Oppdatering er kjørt med ukjent resultat")
+
+except requests.exceptions.HTTPError as error:
+    print("Fant ikke status for workflow i tms-mikrofrontend-selector.{}".format(error))
+    sys.exit(0)
+except Exception as error:
+    print("Feil i henting av status: {}".format(error))
+    sys.exit(0)
